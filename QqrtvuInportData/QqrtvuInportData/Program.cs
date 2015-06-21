f03497bfadc87c6951eb92a7073d3cd6574b99ce
applenele <1941973283@qq.com>
2015-06-21 23:50:58 +0800

    1

--- QqrtvuInportData/QqrtvuInportData/Program.cs
+++ QqrtvuInportData/QqrtvuInportData/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,25 @@ namespace QqrtvuInportData
     {
         static void Main(string[] args)
         {
+            string myConn = ConfigurationManager.ConnectionStrings["mysqlsqerver"].ConnectionString;
+
+            //SqlConnection conn = new SqlConnection(myConn);
+
+            //SqlCommand cmd = conn.CreateCommand();
+
+            //cmd.CommandText = "select * from Users";
+            string sql = "select * from Users";
+            DataSet ds = SqlHelper.ExecuteSqlDataSet(sql, CommandType.Text);
+
+            foreach (DataRow mDr in ds.Tables[0].Rows)
+            {
+                foreach (DataColumn mDc in ds.Tables[0].Columns)
+                {
+                    Console.WriteLine(mDr[mDc].ToString());
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
